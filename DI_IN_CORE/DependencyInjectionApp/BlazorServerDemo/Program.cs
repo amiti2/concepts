4413b20594aa2117afa5ce12ab9c4668f7b2bb3a
amiti2 <arcirodha1@gmail.com>
2023-07-10 05:52:23 +0530

    Looping through multiple implementations

--- DI_IN_CORE/DependencyInjectionApp/BlazorServerDemo/Program.cs
+++ DI_IN_CORE/DependencyInjectionApp/BlazorServerDemo/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<WeatherForecastService>();
+builder.Services.AddSingleton<IDemo, Demo>();
 builder.Services.AddSingleton<IDemo, UtcDemo>();
 builder.Services.AddSingleton<ProcessDemo>();
 
